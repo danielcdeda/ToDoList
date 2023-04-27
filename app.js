d27ba5c3a12f7d6d11581674f8cb93d0d76cd715
DanielDeda <daniel.costa.deda.oliveira@gmail.com>
2023-04-27 16:01:00 -0300

    v2

--- app.js
+++ app.js
@@ -4,8 +4,16 @@ const bodyParser = require("body-parser");
 const app = express();
 
 app.get("/", function(req, res){
-    res.send("Hello!")
-})
+    var today = new Date;
+
+    if (today.getDay() == 6 || today.getDay() == 0) {
+        res.send("Ae, é final de semana!")
+    }
+
+    else {
+        res.send("Dia de trabalho!")
+    }
+});
 
 app.listen(2048, function(){
     console.log("working on port 2048!")
