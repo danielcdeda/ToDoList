1b150ef22b33d139f0836842a3dc51efcc7a398a
DanielDeda <daniel.costa.deda.oliveira@gmail.com>
2023-05-04 15:17:22 -0300

    update route /work

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const bodyParser = require("body-parser");
 const app = express();
  
 let items = ["Buy Food", "Eat Food", "Cook Food"];
+let workItems = [];
  
 app.set('view engine', "ejs");
  
@@ -25,21 +26,38 @@ app.get("/", function (req, res) {
  
  
   res.render("list", {
-    kindOfDay: day,
+    listTitle: day,
     newListItems: items
   });
  
 });
  
 app.post("/", function (req, res) {
-  var item = req.body.newItem;
- 
-  items.push(item);
- 
-  res.redirect("/");
+
+  let item = req.body.newItem;
+
+  if (req.body.list === "Work") {
+    workItems.push(item);
+    res.redirect("/work");
+  }
+
+  else {
+    items.push(item);
+    res.redirect("/");
+  }
  
 });
- 
+
+app.get("/work", function(req, res){
+  res.render("list", {listTitle: "Work List", newListItems: items})
+})
+
+app.post("/work", function(req, res){
+  let item = req.body.newItem;
+  workItems.push(item);
+  
+})
+
 app.listen(2048, function () {
   console.log("Server started on port 3000.");
 });
