1d7ea49f102a13a5a5805b919ac45075707434c2
DanielDeda <daniel.costa.deda.oliveira@gmail.com>
2023-05-03 21:51:26 -0300

    added css

--- app.js
+++ app.js
@@ -3,25 +3,25 @@ const bodyParser = require("body-parser");
  
 const app = express();
  
-var items = ["Buy Food", "Eat Food", "Cook Food"];
+let items = ["Buy Food", "Eat Food", "Cook Food"];
  
 app.set('view engine', "ejs");
  
-app.use(bodyParser.urlencoded({
-  extended: true
-}));
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.use(express.static("public"))
  
 app.get("/", function (req, res) {
  
-  var today = new Date();
+  let today = new Date();
  
-  var options = {
+  let options = {
     weekday: "long",
     day: "numeric",
     month: "long",
   };
  
-  var day = today.toLocaleDateString("en-US", options);
+  let day = today.toLocaleDateString("en-US", options);
  
  
   res.render("list", {
